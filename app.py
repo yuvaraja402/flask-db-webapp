1da9cf736e64d685e48647bc06cca2273e9fa572
Yuvaraja Panneer <Yuvaraja.Panneerselvam@ibm.com>
2023-03-16 21:53:48 +0000

    adjusted app.py

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ from flask import request
 app = Flask(__name__)
 
 username = []
-sorted = {}
 
 @app.route('/', methods =["GET", "POST"])
 def homepage():
     if request.method == 'POST':
         username.append(request.form['name'])
         sorted = set(username)
-    return render_template('page.html',username=username,sorted = sorted)
+    return render_template('page.html',username=username,sorted=sorted)
 
 
 if __name__ == "__main__":
